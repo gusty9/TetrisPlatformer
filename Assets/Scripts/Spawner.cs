e65283024d543a918eed318606fceda4f294a5e5
Erik Gustafson <erikgustafson09@gmail.com>
2022-02-23 22:31:11 -0500

    made aseprite workflow work. animated stick figure - beggining to animate main character

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -11,7 +11,7 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        spawnNext();
+        //spawnNext();
     }
 
     // Update is called once per frame
